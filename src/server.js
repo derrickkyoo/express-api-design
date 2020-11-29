e818147cbf66100333c07a0ffc9bb6570c1b68dd
Derrick Yoo <4998684+derrickyoo@users.noreply.github.com>
2020-11-29 10:32:22 -0700

    Add basic routes and example middleware

--- src/server.js
+++ src/server.js
@@ -7,9 +7,30 @@ export const app = express()
 
 app.disable('x-powered-by')
 
+// Middleware
 app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+// Custom middleware
+const log = (req, res, next) => {
+  console.log('Custom middleware')
+  next()
+}
+
+// Example route
+app.get('/', log, (req, res) => {
+  res.send({ message: 'hello' })
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.send({ message: 'ok' })
+})
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('Server listening on port: 3000')
+  })
+}
